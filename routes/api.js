9541f9dd6323b29cde865118caf55ed1ca02a920
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-23 16:43:39 -0500

    finished dev of post /translate
    added error handling for response

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,21 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      console.log('testing post /translate req.body ', req.body);
+      //console.log('testing post /translate req.body ', req.body);
       let text = req.body.text,
           locale = req.body.locale,
           translated = translator.translate(text, locale);
 
-    console.log('testing post /translate variables ', text, locale, translated);
-    res.json({'text': text, 'translation': translated});
+    //console.log('testing post /translate variables ', text, locale, translated);
+    if (locale && locale !== 'american-to-british' && locale !== 'british-to-american') {
+      res. json({error: 'Invalid value for locale field'});
+    } else if (text === '') {
+      res.json({error: 'No text to translate'});
+    } else if (!locale || !text) {
+      res.json({error: 'Required field(s) missing'});
+    } else {
+      res.json({'text': text, 'translation': translated});
+    }
+    
     });
 };
