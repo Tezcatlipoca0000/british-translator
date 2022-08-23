ba4b0febbeb2b827d6f2f7dac0a5be3db4814b2e
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-23 16:55:08 -0500

    began unit test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,5 +4,16 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
+    const translator = new Translator();
+
+    test('Translate Mangoes are my favorite fruit. to British English', function() {
+        let text = 'Mangoes are my favorite fruit.',
+            locale = 'american-to-british',
+            translated = translator.translate(text, locale);
+
+        assert.isString(translated, 'translated is a string');
+        assert.include(translated, 'favourite', 'string includes the translation');
+        assert.notInclude(translated, 'favorite', 'string does not include original term');
+    });
 
 });
