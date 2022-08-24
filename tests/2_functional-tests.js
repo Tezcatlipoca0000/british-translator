6775e12cc9b961b020c44544ad427ba05dc1cff3
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-23 22:31:17 -0500

    dev functional test 1

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,25 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    const translator = new Translator();
+
+    test('Translation with text and locale fields: POST request to /api/translate', function(done) {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .type('form')
+            .send({
+                text: 'Mangoes are my favorite fruit.',
+                locale: 'american-to-british'
+            })
+            .end(function(err, res) {
+                assert.isNull(err, 'error is null');
+                assert.equal(res.status, 200, 'res.status is 200');
+                assert.isString(res.body.translation, 'translation is a string');
+                assert.include(res.body.translation, 'favourite', 'string includes the translation');
+                assert.notInclude(res.body.translation, 'favorite', 'string does not include original term');
+                done();
+            });
+    });
 
 });
